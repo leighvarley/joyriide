d72fac795ff478c5c766f87d2fdeabe2784bef03
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-09-27 00:56:32 -0400

    added js file, learn more page, and HR page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
 
+  $("#learnpage").hide();
+  $("#hrpage").hide();
+
+  $("#learnbutton").click(function(){
+      $("#learnpage").show();
+      $("#home").hide();
+      $("#hrpage").hide();
+  });
+
+  $("#hrbutton").click(function(){
+      $("#hrpage").show();
+      $("#home").hide();
+      $("#learnpage").hide();
+  });
+
+});
