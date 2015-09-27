557bdd55a6326f666ad2a62e5d9ca472b0df4b3f
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-09-27 01:28:05 -0400

    added navbar

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 
+  $("#navbar").hide();
   $("#learnpage").hide();
   $("#hrpage").hide();
 
   $("#learnbutton").click(function(){
+      $("#navbar").show();
       $("#learnpage").show();
       $("#home").hide();
       $("#hrpage").hide();
   });
 
   $("#hrbutton").click(function(){
+      $("#navbar").show();
       $("#hrpage").show();
       $("#home").hide();
       $("#learnpage").hide();
