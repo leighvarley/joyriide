f18e980aedd1d1b6e060f78ffec1ef5d5ab6a2e1
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-09-28 11:37:51 -0400

    broke out page files

--- js/app.js
+++ js/app.js
@@ -1,33 +1,41 @@
 $(document).ready(function() {
 
+  // $("#navbar").hide();
+  // $("#learnpage").hide();
+  // $("#hrpage").hide();
+  // $("#about").hide();
+  // $("#howitworksinfo").hide();
+  //
+  // $("#learnbutton").click(function(){
+  //     $("#navbar").show();
+  //     $("#learnpage").show();
+  //     $("#home").hide();
+  //     $("#hrpage").hide();
+  //     $("#howitworksinfo").hide();
+  //     $("#about").hide();
+  // });
+  //
+  // $("#hrbutton").click(function(){
+  //     $("#navbar").show();
+  //     $("#hrpage").show();
+  //     $("#home").hide();
+  //     $("#learnpage").hide();
+  //     $("#about").hide();
+  //     $("#howitworksinfo").hide();
+  //
+  // });
+
+  $("#learnbutton").click(function(){
+      $("#howitworksinfo").hide();
+  });
+
   function toggle() {
   	for ( var i=0; i < arguments.length; i++ ) {
   		$(arguments[i]).style.display = ($(arguments[i]).style.display != 'none' ? 'none' : '' );
   	}
   }
 
-  $("#navbar").hide();
-  $("#learnpage").hide();
-  $("#hrpage").hide();
-  $("#about").hide();
 
-  $("#learnbutton").click(function(){
-      $("#navbar").show();
-      $("#learnpage").show();
-      $("#home").hide();
-      $("#hrpage").hide();
-      $("#howitworksinfo").hide();
-      $("#about").hide();
-  });
-
-  $("#hrbutton").click(function(){
-      $("#navbar").show();
-      $("#hrpage").show();
-      $("#home").hide();
-      $("#learnpage").hide();
-      $("#about").hide();
-
-  });
 
   $("#howitworks").click(function(){
       $("#howitworksinfo").toggle();
